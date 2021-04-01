273825b6b8f2dcaccca788e03f24e17fbfa73499
Yan <yanyu5025@gmail.com>
2021-04-02 00:11:12 +0200

    adding files

---
+++ Animation/sketch.js
@@ -0,0 +1,19 @@
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  strokeWeight(1);
+
+}
+
+function draw() {
+  background(255,255,255,);
+  stroke(121,33,255);
+  line(0, mouseY, width, mouseY,);
+  line(mouseX, 0, mouseX,height,);
+  ellipseColor = color(121,33,255);
+  ellipseColor.setAlpha(250 + 150 * sin(millis() / 1000));
+  fill(ellipseColor);
+  noStroke(0);
+  ellipse(mouseX, mouseY, 50 + 5 * sin(frameCount / 20));
+}
+
