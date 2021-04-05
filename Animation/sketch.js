bf2582ab07194eb0cfb38e873e725013200202e5
Yan <yanyu5025@gmail.com>
2021-04-05 13:30:32 +0200

    changed displayWidth and displayHeight

--- Animation/sketch.js
+++ Animation/sketch.js
@@ -6,10 +6,11 @@ function setup() {
 }
 
 function draw() {
-  background(255,255,255,);
+    clear();
+//  background(255,255,255,);
   stroke(121,33,255);
-  line(0, mouseY, width, mouseY,);
-  line(mouseX, 0, mouseX,height,);
+  line(0, mouseY, width, mouseY);
+  line(mouseX, 0, mouseX,height);
   ellipseColor = color(121,33,255);
   ellipseColor.setAlpha(250 + 150 * sin(millis() / 1000));
   fill(ellipseColor);
